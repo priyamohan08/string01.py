2b9be0604c344b4bf68b625e1603c6bfc71fee4d
priyamohan08 <109510877+priyamohan08@users.noreply.github.com>
2022-07-21 15:07:21 +0530

    Add files via upload

---
+++ string01.py
@@ -0,0 +1,44 @@
+# print(type(2.56))
+# print(type(5))
+
+# print('hello world')
+# print('''hello world''')
+# print("hello world")
+# print("""hello world""")
+
+
+# '''working withint,float and str'''
+#  print(54+2.1)
+#  print(type(26+3.8))
+#  print(type(6.23+3.25))
+#  print(type(6.4+2.6))
+#  print('256'+2.6)
+#  print('54'+54)
+
+# print('hello'+54)
+# print('hello'+2.5)
+
+# print(1,2,3,4,5,6)
+#print(1,2,3,4,5,sep='')
+#print(1,2,3,4,5,sep=' ')
+#print(1,2,3,4,5,sep='   ')
+#print(1,2,3,4,5,sep='-')
+
+# print('hello'+'world')
+# print('hello '+'world')
+# print('hello'+' world')
+# print('hello','world')
+# print('hi','priya')
+
+# print('hello'+''+'priya')
+# print('hello'+' '+'priya')
+# print("hello"+" "+"priya")
+# print("hello"+"-"+"priya")
+
+# print('priya'*4)
+# print('priya '*4)
+# print('priya\n'*4)
+# print('priya\n'*4,end='')
+# print('priya\n'*7'end=' ')
+# print('priya\n',end='')
+# print('priya\n',sep='')
